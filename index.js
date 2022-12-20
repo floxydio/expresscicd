2f9cf13aad620b59f960edb18ce952b5836c526a
floxydio <diooktar@gmail.com>
2022-12-20 09:15:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/product", function (req, res) {
+  res.send({
+    message: "Init Test",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server Running on");
+});
